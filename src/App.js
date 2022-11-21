92307473a3e50e1955ce8087a1c8f9ffd1e67575
LeenaDev32 <bleenaweb238@gmail.com>
2022-11-21 17:25:35 +0530

    update table data and fix UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomizedTables from './Component/TableData';
+
+function App() {
+  return (
+    <div className="App">
+     <CustomizedTables />
+    </div>
+  );
+}
+
+export default App;
